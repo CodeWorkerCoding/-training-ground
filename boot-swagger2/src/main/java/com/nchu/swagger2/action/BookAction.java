15f6e3de261a5cce8ef46a6ba226f90427cc70e7
jianjianfu <jianjian.fu@shenmajr.com>
2017-12-09 12:10:27 +0800

    boot 集成swagger项目代码检入

--- boot-swagger2/src/main/java/com/nchu/swagger2/action/BookAction.java
+++ boot-swagger2/src/main/java/com/nchu/swagger2/action/BookAction.java
@@ -1,8 +1,12 @@
 package com.nchu.swagger2.action;
 
 import com.nchu.swagger2.model.pojo.Book;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.*;
 
@@ -11,6 +15,7 @@ import java.util.*;
  * @project self-learn
  * @date 2017/12/8 11:33
  */
+@Api(value = "图书相关接口", protocols = "https")
 @RestController
 @RequestMapping("/book")
 public class BookAction {
@@ -18,7 +23,7 @@ public class BookAction {
     private static Map<String, Book> bookMap = Collections.synchronizedMap(new HashMap<String, Book>());
 
 
-    @ApiOperation(value = "获取数据列表", notes = "测试List")
+    @ApiOperation(value = "获取数据列表", notes = "测试List", response = List.class)
     @GetMapping("/list")
     public List<Book> list() {
         List<Book> list = new ArrayList<>(bookMap.values());
@@ -26,9 +31,19 @@ public class BookAction {
     }
 
     @ApiOperation(value = "新增一本书", notes = "测试add")
+    @ApiImplicitParams(
+            @ApiImplicitParam(name = "book", value = "书本实体", required = true, dataType = "Book", paramType = "RequestBoy")
+    )
     @PostMapping("add")
     public String addBook(@RequestBody Book book) {
         bookMap.put(book.getId(), book);
+        return "success";
+    }
+
+    @ApiIgnore
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable String id) {
+
         return "success";
     }
 
